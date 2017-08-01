cfdefd905a492e9d254a24cb44267d0ef72fdd58
Quinn <qpm3@mit.edu>
2017-07-31 23:23:19 -0400

    Initial commit

---
+++ app/src/main/java/com/magendanz/villanovamec/MainActivity.java
@@ -0,0 +1,13 @@
+package com.magendanz.villanovamec;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
