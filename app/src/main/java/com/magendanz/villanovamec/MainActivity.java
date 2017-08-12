d48a0bd85c078d41332f948067d7d4f74abf8680
Quinn <qpm3@mit.edu>
2017-08-11 23:14:24 -0400

    Add maps, improve UI

--- app/src/main/java/com/magendanz/villanovamec/MainActivity.java
+++ app/src/main/java/com/magendanz/villanovamec/MainActivity.java
@@ -1,58 +1,80 @@
 package com.magendanz.villanovamec;
 
+import android.app.FragmentManager;
+import android.content.res.Resources;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
+import android.app.FragmentTransaction;
 import android.view.View;
 
-public class MainActivity extends FragmentActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-    private static final float TAB_FADE = .8f;
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private static final float TAB_FADE = .5f;
 
     private ScheduleFragment scheduleFragment;
     private NewsFragment newsFragment;
-    private MapFragment mapFragment;
+    private MapFragment mappingFragment;
+    private Resources res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        res = getResources();
         scheduleFragment = new ScheduleFragment();
         newsFragment = new NewsFragment();
-        mapFragment = new MapFragment();
-        findViewById(R.id.schedule_button).setAlpha(1);
-        findViewById(R.id.news_button).setAlpha(TAB_FADE);
-        findViewById(R.id.map_button).setAlpha(TAB_FADE);
-        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, scheduleFragment).commit();
+        mappingFragment = MapFragment.newInstance();
+        setTabFormat(findViewById(R.id.schedule_button));
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, scheduleFragment);
+        fragmentTransaction.commit();
+    }
+
+    public void onMapReady(GoogleMap googleMap){
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(res.getDimension(R.dimen.villanova_lat), res.getDimension(R.dimen.villanova_lng)))
+                .title("Marker"));
     }
 
     public void switchToMap(View view){
-        findViewById(R.id.map_button).setAlpha(1);
-        findViewById(R.id.news_button).setAlpha(TAB_FADE);
-        findViewById(R.id.schedule_button).setAlpha(TAB_FADE);
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.fragment_container, mapFragment);
+        setTabFormat(view);
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, mappingFragment);
         transaction.addToBackStack(null);
         transaction.commit();
     }
 
     public void switchToNews(View view){
-        findViewById(R.id.news_button).setAlpha(1);
-        findViewById(R.id.map_button).setAlpha(TAB_FADE);
-        findViewById(R.id.schedule_button).setAlpha(TAB_FADE);
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        setTabFormat(view);
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
         transaction.replace(R.id.fragment_container, newsFragment);
         transaction.addToBackStack(null);
         transaction.commit();
     }
 
     public void switchToSchedule(View view){
-        findViewById(R.id.schedule_button).setAlpha(1);
-        findViewById(R.id.map_button).setAlpha(TAB_FADE);
-        findViewById(R.id.news_button).setAlpha(TAB_FADE);
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        setTabFormat(view);
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
         transaction.replace(R.id.fragment_container, scheduleFragment);
         transaction.addToBackStack(null);
         transaction.commit();
     }
+
+    private void setTabFormat(View view){
+        findViewById(R.id.schedule_button).setAlpha(TAB_FADE);
+        findViewById(R.id.map_button).setAlpha(TAB_FADE);
+        findViewById(R.id.news_button).setAlpha(TAB_FADE);
+        view.setAlpha(1);
+        findViewById(R.id.schedule_button_frame).setBackgroundColor(res.getColor(R.color.transparent));
+        findViewById(R.id.map_button_frame).setBackgroundColor(res.getColor(R.color.transparent));
+        findViewById(R.id.news_button_frame).setBackgroundColor(res.getColor(R.color.transparent));
+        ((View) view.getParent()).setBackgroundColor(res.getColor(R.color.selected_tab_fade));
+    }
+
 }
--- app/src/main/java/com/magendanz/villanovamec/NewsFragment.java
+++ app/src/main/java/com/magendanz/villanovamec/NewsFragment.java
@@ -1,9 +1,8 @@
 package com.magendanz.villanovamec;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +16,7 @@ public class NewsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_schedule, container, false);
+        return inflater.inflate(R.layout.fragment_news, container, false);
     }
 
 
